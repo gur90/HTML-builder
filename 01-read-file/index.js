3619fae650bb512344aede2646e735cadb8d61ee
gur90 <annettgur@gmail.com>
2022-05-23 15:58:05 +0300

    fix: fix creating index.html in  6th task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,10 +6,3 @@ let data = '';
 readebleStream.on('data', chunk => data+=chunk)
 readebleStream.on('end',()=>  console.log(data))
 
-/*fs.readFile(
-    path.join(__dirname, "text.txt"), "utf8",
-    function(error, data){
-        console.log('async');
-        if(error) throw error;
-        console.log(data)
-    })*/
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,76 +1,14 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 const path = require('path')
-const fsPromises = fs.promises;
+const fsPromises = require('fs/promises');
 const http = require("http");
 const index= path.join(__dirname, 'project-dist', 'index.html')
 const { stdin, stdout} = process;
 const bundle= path.join(__dirname, 'project-dist','style.css')
 const styles =path.join(__dirname, 'styles')
-async function style(){
-    try{
-    const files = await fs.readdir(styles, {
-        withFileTypes:true
-    })
-    let arr =[]
-        for(const file of files)
-            if(file.isFile() && path.extname(file.name)=='.css'){
-            const st = path.join(styles, file.name)
-            const reading = await fs.readFile(st) 
-            arr.push(reading.toString())
-            }    
-            //console.log(arr)     
-        await fs.writeFile(bundle, arr.join('\n'))
-    }catch (err){ console.log(err)}
-}
-style()
-fs.mkdir(path.join(__dirname,'project-dist'), {recursive:true} ).then( function () {
-    console.log( 'Directory created successfully' );
-    }). catch ( function () {
-    console.log( 'failed to create directory' );
-    });
-const dir = path.join(__dirname, 'project-dist/assets')
-const from = path.join(__dirname, 'assets')
-const to = path.join(__dirname, 'project-dist/assets')
-fs.mkdir(path.join(__dirname,'project-dist', 'assets'), {recursive:true} ).then( function () {
-    console.log( 'Directory created successfully' );
-    }). catch ( function () {
-    console.log( 'failed to create directory' );
-    });
-fs.mkdir(path.join(__dirname,'project-dist', 'assets', 'fonts'), {recursive:true} ).then( function () {
-        console.log( 'Directory created successfully' );
-        }). catch ( function () {
-        console.log( 'failed to create directory' );
-        });
-fs.mkdir(path.join(__dirname,'project-dist', 'assets', 'img'), {recursive:true} ).then( function () {
-            console.log( 'Directory created successfully' );
-            }). catch ( function () {
-            console.log( 'failed to create directory' );
-            });
-fs.mkdir(path.join(__dirname,'project-dist', 'assets', 'svg'), {recursive:true} ).then( function () {
-                console.log( 'Directory created successfully' );
-                }). catch ( function () {
-                console.log( 'failed to create directory' );
-                });
-async function copyDir(from, to){
-    try {
-        const fold = await fs.readdir(from, {withFileTypes:true});
-        for(let fol of fold){
-            const main = path.join(from, fol.name)
-            const target = path.join (to, fol.name)
-            if(fol.isDirectory()){
-                await copyDir(main, target)
-            }else {
-                await fs.copyFile(main, target)
-            }
-        }
-    } catch (error) {
-        console.error(error)
-    }
-}
-copyDir(from, to)
 
-fs.readFile(
-        path.join(__dirname, "template.html"), "utf8", function(error, data){
+
+fs.readFile(path.join(__dirname, "template.html"), "utf8", function(error, data){
             if(error) throw error;
             let templ = data
             //console.log(templ)
@@ -80,6 +18,7 @@ fs.readFile(
                         if (err) throw err;
                         templ = templ.replace('{{header}}', data);
                         //console.log(templ)
+                       
                     })
                 }
                 if(templ.includes('{{articles}}')){
@@ -87,14 +26,15 @@ fs.readFile(
                         if (err) throw err;
                         templ = templ.replace('{{articles}}', dataAr);
                         //console.log(templ)
+                        
                     })
                 }
                 if(templ.includes('{{footer}}')){
-                    fs.readFile(path.join(__dirname, 'components', 'footer.html'), 'utf8', function(err, dataF){
+                     fs.readFile(path.join(__dirname, 'components', 'footer.html'), 'utf8', function(err, dataF){
                         if (err) throw err;
                         templ = templ.replace('{{footer}}', dataF);
                         //console.log(templ)
-                        fs.writeFile(index, templ, function(err){
+                        fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), templ, function(err){
                             if(err)throw err;
                             console.log('done')
                         })
@@ -103,7 +43,69 @@ fs.readFile(
            
         })    
 
-       
+        async function style(){
+            try{
+            const files = await fsPromises.readdir(styles, {
+                withFileTypes:true
+            })
+            let arr =[]
+                for(const file of files)
+                    if(file.isFile() && path.extname(file.name)=='.css'){
+                    const st = path.join(styles, file.name)
+                    const reading = await fsPromises.readFile(st) 
+                    arr.push(reading.toString())
+                    }    
+                    //console.log(arr)     
+                await fsPromises.writeFile(bundle, arr.join('\n'))
+            }catch (err){ console.log(err)}
+        }
+        style()
+        fsPromises.mkdir(path.join(__dirname,'project-dist'), {recursive:true} ).then( function () {
+            console.log( 'Directory created successfully' );
+            }). catch ( function () {
+            console.log( 'failed to create directory' );
+            });
+        const dir = path.join(__dirname, 'project-dist/assets')
+        const from = path.join(__dirname, 'assets')
+        const to = path.join(__dirname, 'project-dist/assets')
+        fsPromises.mkdir(path.join(__dirname,'project-dist', 'assets'), {recursive:true} ).then( function () {
+            console.log( 'Directory created successfully' );
+            }). catch ( function () {
+            console.log( 'failed to create directory' );
+            });
+        fsPromises.mkdir(path.join(__dirname,'project-dist', 'assets', 'fonts'), {recursive:true} ).then( function () {
+                console.log( 'Directory created successfully' );
+                }). catch ( function () {
+                console.log( 'failed to create directory' );
+                });
+        fsPromises.mkdir(path.join(__dirname,'project-dist', 'assets', 'img'), {recursive:true} ).then( function () {
+                    console.log( 'Directory created successfully' );
+                    }). catch ( function () {
+                    console.log( 'failed to create directory' );
+                    });
+        fsPromises.mkdir(path.join(__dirname,'project-dist', 'assets', 'svg'), {recursive:true} ).then( function () {
+                        console.log( 'Directory created successfully' );
+                        }). catch ( function () {
+                        console.log( 'failed to create directory' );
+                        });
+              
+        async function copyDir(from, to){
+            try {
+                const fold = await fsPromises.readdir(from, {withFileTypes:true});
+                for(let fol of fold){
+                    const main = path.join(from, fol.name)
+                    const target = path.join (to, fol.name)
+                    if(fol.isDirectory()){
+                        await copyDir(main, target)
+                    }else {
+                        await fsPromises.copyFile(main, target)
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        copyDir(from, to)
             
            
 
